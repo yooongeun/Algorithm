0f83ce2729b759bd0de7cfcf5f12f56b3024b81e
Youngeun <122420156+yooongeun@users.noreply.github.com>
2024-08-30 21:45:25 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,10 @@
-public class Solution {
+class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        while(n > 0){
-            answer += n%10;
-            n/=10;
+        while( n > 0){
+            answer += n % 10;
+            n = n / 10;
         }
         return answer;
     }
