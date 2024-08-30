a58f6a538fbf9e291b8fc7237c4eb30227eba704
Youngeun <122420156+yooongeun@users.noreply.github.com>
2024-08-30 22:10:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.53 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,17 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = -999999999;
-        int max = 0; 
+        int answer = 0; 
+        /*
+        1. 정렬하기
+        2. 양끝에 두개 곱하기
+        3. 두개 비교해서 큰거 반환하기
+        */
+        Arrays.sort(numbers);
+        int a = numbers[0] * numbers[1];
+        int b = numbers[numbers.length - 1] * numbers[numbers.length - 2];
         
-        for(int i = 0; i < numbers.length; i++){
-            for(int j = numbers.length - 1; j > i; j--){
-                max = numbers[i] * numbers[j];
-                if(max > answer){
-                    answer = max;
-                }
-            }
+        if(a > b){
+            answer = a;
+        }else{
+            answer = b;
         }
-
+        
         return answer;
     }
 }
