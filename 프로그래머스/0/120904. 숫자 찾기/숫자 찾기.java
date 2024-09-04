eb17885ccbfc7c393ea513601dfaa96eedb7ea8f
Youngeun <122420156+yooongeun@users.noreply.github.com>
2024-09-05 02:50:18 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,20 +1,9 @@
-import java.util.*;
-
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        int count = 0;
-        String str = String.valueOf(num);
-        char word = Character.forDigit(k, 10); 
-        
-        for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == word && count == 0){
-                answer = i + 1; 
-                count++;
-            }else if(answer == 0){
-                answer = -1; 
-            }
-        }
-        return answer;
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+
+        int answer = numStr.indexOf(kStr);
+        return answer < 0 ? -1 : answer + 1 ;
     }
 }
