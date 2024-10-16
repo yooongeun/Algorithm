47e7e8eda2945cfdce40f5461f06eac6408d3ef1
Youngeun <122420156+yooongeun@users.noreply.github.com>
2024-10-16 17:15:47 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        if(a % 2 != 0 && b % 2 != 0){
+            answer = (a * a) + (b * b);
+        }else if(a % 2 != 0 || b % 2 != 0){
+            answer = 2 * (a + b);
+        }else{
+            answer = a - b;
+        }
+        
+        answer = answer > 0 ? answer : answer * -1;
+        
+        return answer;
+    }
+}
